afef9e1b0ad1fe40e174f14e4ba0aefe7ee47e1e
ilevisvariedades-ksonaki <ilevisvariedades@gmail.com>
2026-01-18 21:56:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import os
+from flask import Flask
+import telebot
+from threading import Thread
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot IlevisVariedades Online!"
+
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Bot IlevisVariedades funcionando!")
+
+def run_bot():
+    bot.infinity_polling()
+
+if __name__ == "__main__":
+    Thread(target=run_bot).start()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
